a53a07829c566ef2936006891750de8f14ae7a4a
jannalu <84465297+jannalu@users.noreply.github.com>
2024-01-26 10:52:08 -0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CameraFunc
+//
+//  Created by Janna Lu on 1/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapButton(){
+        
+    }
+
+
+}
+
